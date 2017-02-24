19d6907e44d804b1bdf2f2c0b321b093f9ed3c0d
David Barrat <david.barrat.1@gmail.com>
2017-02-24 03:02:37 +0100

    Update Cards

--- app/app.js
+++ app/app.js
@@ -1,1 +1,11 @@
-var App = angular.module('App', ['ngRoute', 'spotify', 'ngTagsInput','ui.bootstrap']);
+var App = angular.module('App', ['ngRoute', 'spotify', 'ngTagsInput','ui.bootstrap','angular-cardflow']);
+
+App.directive("deferredCloak", function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            attrs.$set("deferredCloak", undefined);
+            element.removeClass("deferred-cloak");
+        }
+    };
+});
--- app/controllers/Controller.js
+++ app/controllers/Controller.js
@@ -2,15 +2,8 @@ App.controller('MainCtrl', [
   '$scope', '$timeout', '$http',
   function ($scope, $timeout,$http) {
 
-    //console.log("in controller")
-
     $scope.tags = [];
-
     $scope.loadAlbums = function($query) {
-
-      //alert($query)
-
-      //$http.get('https://api.spotify.com/v1/search/'+$query, { cache: true, data: { q: $query,type: 'album'} })
 
         if($query && $query.length > 0)
         {
@@ -20,176 +13,65 @@ App.controller('MainCtrl', [
                 url : "https://api.spotify.com/v1/search?q=" + escape($query) + "&type=album",
                 cache: true,
                 responseType: "json"
-                // data: {
-                //   q: $query,
-                //   type: 'album'
-                // }
               }).then(function(response, status) {
 
-                var albums = response.data.albums.items;
+                var albums = response.data.albums !== undefined ? response.data.albums.items : [];
                 var ret = [];
 
                 var isIn = function(arr,val)
                 {
                   for(var j=0; j<arr.length; j++)
                   {
-                    if(arr[j].name === val)
-                    {
-                      return true;
-                    }
+                    if(arr[j].name === val){return true;}
                   }
                   return false;
                 }
 
-
-                if(albums)
+                if(albums.length > 0)
                 {
-
                   for(var i=0; i < albums.length; i++)
                   {
-                    console.log(albums[i])
-
                     if(isIn(ret,albums[i].name) === false){
                       ret.push({
                         "name":albums[i].name,
-                        "image": albums[i].images[2].url,
+                        "thumbnail": albums[i].images[2].url,
+                        "cover": albums[i].images[0].url,
                         "id": albums[i].id
                       })
                     }
                   }
-
-                  console.log(ret)
-
-
                   return ret;
                 }
                 else {
                   return []
                 }
               })
-              // .then(function(response) {
-              // //console.log(response)
-              // var albums = response.data.albums;
-              //
-              // //console.log(albums)
-              // return albums.filter(function(album) {
-              //       console.log(album)
-              //       return true;
-              //       //return country.name.toLowerCase().indexOf($query.toLowerCase()) != -1;
-              // })
-
-              // return albums.filter(function(album) {
-              //       console.log(album)
-              //       //return country.name.toLowerCase().indexOf($query.toLowerCase()) != -1;
-              // }
-              //   return country.name.toLowerCase().indexOf($query.toLowerCase()) != -1;
-              //});
-              //var countries = response.data;
-              // var country = $scope.countries;
-              // return countries.filter(function(country) {
-              //   return country.name.toLowerCase().indexOf($query.toLowerCase()) != -1;
-              //});
         }
         else {
           return []
         }
-
-      //   return $scope.countries.filter(function(country) {
-      //     return country.name.toLowerCase().indexOf($query.toLowerCase()) != -1;
-      //   });
-       //
-       //
-       //
-      //  return $scope.countries.filter(function(country) {
-      //    return country.name.toLowerCase().indexOf($query.toLowerCase()) != -1;
-      //  });
-
-
-
-
     };
 
+    $scope.cardflow={};
+      // I use a timeout so I have access to all the elements
+      $timeout(function(){
+          $scope.$watch(function(){ return $scope.cardflow.current; }, function(){
+              console.log($scope.cardflow);
+          });
+      }, 100);
 
-    /*$scope.user = {};
-    $scope.friends = {};
-
-    $scope.logged = false;
-    $scope.bye = false;
-    $scope.welcome = false;
-
-    $scope.$watch(
-      function() {
-        return Facebook.isReady();
-      },
-      function(newVal) {
-        if (newVal) {
-          $scope.facebookReady = true;
-        }
+      $scope.cardflow.cards = [];
+      var types = ['cats','fashion','nature','food','abstract','nightlife'];
+      for (var i=0; i<40; i++){
+          var t = types[i % types.length];
+          $scope.cardflow.cards.push({image:'http://lorempixel.com/198/198/' + t + '/'+((i%10)+1), title: t});
       }
-    );
 
-    $scope.IntentLogin = function() {
-      Facebook.getLoginStatus(function (response) {
-        if (response.status == 'connected') {
-          $scope.logged = true;
-          $scope.me();
-        } else {
-          $scope.login();
-        }
-      });
-    };
-
-    $scope.login = function() {
-      Facebook.login(function (response) {
-        if (response.status == 'connected') {
-          $scope.logged = true;
-          $scope.me();
-          $scope.getFriends();
-        }
-      });
-    };
-
-    $scope.me = function() {
-      Facebook.api('/me', function (response) {
-        $scope.$apply(function () {
-          $scope.user = response;
-        });
-      });
-    };
-
-    $scope.getFriends = function() {
-      Facebook.api('/me/friends?fields=name,location', function (response) {
-        $scope.$apply(function () {
-          $scope.friends = response;
-        });
-      });
-    };
-
-    $scope.logout = function() {
-      Facebook.logout(function () {
-        $scope.$apply(function () {
-          $scope.user = {};
-          $scope.friends = {};
-          $scope.logged = false;
-        })
-      });
-    };
-
-    $scope.$on('Facebook:statusChange', function (ev, data) {
-      console.log('Status: ', data);
-
-      if (data.status == 'connected') {
-        $scope.$apply(function () {
-          $scope.welcome = true;
-          $scope.bye = false;
-        });
-      } else {
-        $scope.$apply( function() {
-          $scope.welcome = false;
-          $scope.bye = true;
-        });
+      $scope.playAlbum = function(id)
+      {
+        alert(id);
       }
-    });*/
+
 
   }
 ]);
