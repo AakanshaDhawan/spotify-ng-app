de1f48966270de156d455dc5a6a9ab268431ccb1
David Barrat <david.barrat.1@gmail.com>
2017-02-26 07:08:34 +0100

    Update ProTractor testing

--- test/scenarios.js
+++ test/scenarios.js
@@ -2,10 +2,17 @@
 
 describe('testing music-search app', function() {
 
-
   it('should automatically redirect to / when location doesn\'t exist', function() {
     browser.get('http://localhost:8000/#/toto');
+    // Check location
     expect(browser.getLocationAbsUrl()).toMatch("/");
+  });
+
+  it('should display app properly, so Title should be visible', function() {
+    browser.get('http://localhost:8000/#/toto');
+    // Checks display App
+    expect(element.all(by.css('h1')).first().getText()).
+    toMatch(/Music Search/);
   });
 
 });
